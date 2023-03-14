1594998a712c63c508f78c55031a9d5f16824f90
mehm8128 <mehm8128@gmail.com>
2023-03-14 23:10:29 +0900

    windicssの導入とhome画面のデザインを入れた

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'virtual:windi.css'
 
 import App from './App.vue'
 import router from './router'
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,7 +17,16 @@ const router = useRouter()
 
 const currentPage = ref(route.query.page ? Number(route.query.page) : 1)
 
-const questions = ref<Question[]>([])
+const questions = ref<Question[]>([
+  {
+    id: 'aaa',
+    question: 'traPaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    answer: 'traPpppppppppppppppppppppppppppppppppppppp',
+    answerer: 'mehm8128',
+    createdAt: '2023-03-03T00:00:00.000Z',
+    updatedAt: '2023-03-03T00:00:00.000Z'
+  }
+])
 
 const questionText = ref('')
 const isSending = ref(false)
@@ -46,32 +55,60 @@ watch(
 </script>
 
 <template>
-  <h1>Questions</h1>
-  <h2>traPに質問してみましょう！</h2>
-  <p>説明説明説明</p>
-  <form :onSubmit="handleSubmitQuestion">
-    <label for="question">質問</label>
-    <textarea
-      v-model="questionText"
-      placeholder="例：部員は何人くらいいますか？、初心者でも大丈夫ですか？"
-      id="question"
-    />
-    <button type="submit" :disabled="isSending">送信</button>
-  </form>
+  <div class="pt-12 mx-auto w-1/2">
+    <h1 class="text-4xl text-center mb-8">Questions</h1>
+    <section>
+      <h2 class="text-2xl">traPに質問してみましょう！</h2>
+      <p>説明説明説明</p>
+      <form :onSubmit="handleSubmitQuestion" class="mx-4 mt-4">
+        <textarea
+          class="border border-gray-300 w-full min-h-32 p-1"
+          v-model="questionText"
+          placeholder="例：部員は何人くらいいますか？、初心者でも大丈夫ですか？"
+          id="question"
+        />
+        <div class="text-right mt-2">
+          <button
+            type="submit"
+            :disabled="isSending"
+            class="bg-blue-300 text-white px-4 py-2 rounded-2xl"
+          >
+            送信
+          </button>
+        </div>
+      </form>
+    </section>
 
-  <h2>最近の質問</h2>
-  <ul>
-    <li v-for="question in questions" :key="question.id">
-      <RouterLink :to="`/questions/${question.id}`">
-        <p>質問：{{ question.question }}</p>
-        <p>回答：{{ question.answer }}</p>
-        <p>回答者：{{ question.answerer }}</p>
-      </RouterLink>
-    </li>
-  </ul>
-  <div>
-    <RouterLink :to="`/?page=${currentPage - 1}`" v-show="currentPage !== 1">前へ</RouterLink>
-    <p>{{ currentPage }}</p>
-    <RouterLink :to="`/?page=${currentPage + 1}`">次へ</RouterLink>
+    <section class="mt-8">
+      <h2 class="text-2xl mb-2">最近の質問</h2>
+      <ul>
+        <li
+          v-for="question in questions"
+          :key="question.id"
+          class="w-full min-h-40 border border-gray-300 p-3"
+        >
+          <p class="border border-gray-400 h-28 p-1 mb-2">{{ question.question }}</p>
+          <details class="mx-4">
+            <summary>回答を表示</summary>
+            <p>{{ question.answer }}</p>
+            <p class="text-right">
+              回答者：
+              <a :href="`https://trap.jp/author/${question.answerer}/`">
+                {{ question.answerer }}
+              </a>
+            </p>
+          </details>
+          <div class="text-right">
+            <RouterLink :to="`/questions/${question.id}`">回答へ></RouterLink>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <div class="flex items-center justify-center gap-4 mt-4">
+      <RouterLink :to="`/?page=${currentPage - 1}`" v-show="currentPage !== 1">前へ</RouterLink>
+      <p>{{ currentPage }}</p>
+      <RouterLink :to="`/?page=${currentPage + 1}`">次へ</RouterLink>
+    </div>
   </div>
 </template>
