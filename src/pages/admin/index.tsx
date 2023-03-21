ba173f32d84cc5cad0fd475dae1d6422b1fb88fc
mehm8128 <mehm8128@gmail.com>
2023-03-21 17:19:41 +0900

    回答がないときにdetailsを表示しないように

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -44,25 +44,27 @@ export default function Admin() {
 								<p className="text-gray-800 leading-tight">
 									{question.question}
 								</p>
-								<details className="mt-2">
-									<summary className="font-medium text-gray-600 cursor-pointer">
-										回答を表示
-									</summary>
-									<p className="mt-2 text-gray-700 leading-tight">
-										{question.answer}
-									</p>
-									<p className="mt-2 text-right text-sm text-gray-500">
-										回答者：
-										<a
-											href={`https://trap.jp/author/${question.answerer}/`}
-											className="underline"
-											target="_blank"
-											rel="noopener noreferrer"
-										>
-											{question.answerer}
-										</a>
-									</p>
-								</details>
+								{question.answer && (
+									<details className="mt-2">
+										<summary className="font-medium text-gray-600 cursor-pointer">
+											回答を表示
+										</summary>
+										<p className="mt-2 text-gray-700 leading-tight">
+											{question.answer}
+										</p>
+										<p className="mt-2 text-right text-sm text-gray-500">
+											回答者：
+											<a
+												href={`https://trap.jp/author/${question.answerer}/`}
+												className="underline"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												{question.answerer}
+											</a>
+										</p>
+									</details>
+								)}
 							</div>
 							<div className="px-6 py-4 bg-gray-100 border-t border-gray-200 flex justify-end rounded-b-md">
 								<Link
