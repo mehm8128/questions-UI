85e1145c5ba5d963980af70dc9e7d6c5261e3dd8
mehm8128 <mehm8128@gmail.com>
2023-03-21 20:28:00 +0900

    fix

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 import { QuestionType } from "@/pages"
 import Navbar from "@/components/Navbar"
+import Question from "@/components/Question"
 
 export default function Admin() {
 	const router = useRouter()
@@ -36,45 +37,7 @@ export default function Admin() {
 				<h2 className="text-2xl font-semibold mb-2">最近の質問</h2>
 				<ul className="space-y-8">
 					{questions.map((question) => (
-						<li
-							key={question.id}
-							className="rounded-md shadow-sm border border-gray-200"
-						>
-							<div className="p-6">
-								<p className="text-gray-800 leading-tight">
-									{question.question}
-								</p>
-								{question.answer && (
-									<details className="mt-2">
-										<summary className="font-medium text-gray-600 cursor-pointer">
-											回答を表示
-										</summary>
-										<p className="mt-2 text-gray-700 leading-tight">
-											{question.answer}
-										</p>
-										<p className="mt-2 text-right text-sm text-gray-500">
-											回答者：
-											<a
-												href={`https://trap.jp/author/${question.answerer}/`}
-												className="underline"
-												target="_blank"
-												rel="noopener noreferrer"
-											>
-												{question.answerer}
-											</a>
-										</p>
-									</details>
-								)}
-							</div>
-							<div className="px-6 py-4 bg-gray-100 border-t border-gray-200 flex justify-end rounded-b-md">
-								<Link
-									className="text-sm font-medium text-blue-500 hover:text-blue-400 transition duration-150 ease-in-out"
-									href={`/admin/answer/${question.id}`}
-								>
-									回答する &rarr;
-								</Link>
-							</div>
-						</li>
+						<Question key={question.id} question={question} isAdmin />
 					))}
 				</ul>
 			</section>
