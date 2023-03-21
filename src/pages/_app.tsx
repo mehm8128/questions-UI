209f4a88d1f584785d6a4884f77463ff7c121d76
mehm8128 <mehm8128@gmail.com>
2023-03-21 20:22:30 +0900

    add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,28 +6,32 @@ import QuestionsHeader from "@/components/QuestionsHeader"
 import { useRouter } from "next/router"
 import axios from "axios"
 import { useEffect } from "react"
+import Head from "next/head"
 
 export default function App({ Component, pageProps }: AppProps) {
 	const router = useRouter()
 	const path = router.pathname
 
-	// useEffect(() => {
-	// 	if (path.startsWith("/admin")) {
-	// 		;(async () => {
-	// 			try {
-	// 				await axios.get(`https://ikura-hamu.trap.show/questions/api/me`, {
-	// 					withCredentials: true,
-	// 				})
-	// 			} catch {
-	// 				document.location =
-	// 					"https://ikura-hamu.trap.show/questions/api/oauth2/authorize"
-	// 			}
-	// 		})()
-	// 	}
-	// }, [path])
+	useEffect(() => {
+		if (path.startsWith("/admin")) {
+			;(async () => {
+				try {
+					await axios.get(`https://ikura-hamu.trap.show/questions/api/me`, {
+						withCredentials: true,
+					})
+				} catch {
+					document.location =
+						"https://ikura-hamu.trap.show/questions/api/oauth2/authorize"
+				}
+			})()
+		}
+	}, [path])
 
 	return (
 		<div className="text-dark-200 bg-[#f7fafc] min-h-screen">
+			<Head>
+				<title>Questions</title>
+			</Head>
 			<QuestionsHeader />
 			<main>
 				<Component {...pageProps} />
